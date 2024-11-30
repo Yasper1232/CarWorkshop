f31125ce83e7080183442831fbd4d6abe7005d22
Kacper <ciaparino123@gmail.com>
2024-11-30 19:50:30 +0100

    testy jednostkowe

---
+++ CarWorkshop.ApplicationTests/ApplicationUser/CurrentUserTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarWorkshop.Application.ApplicationUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CarWorkshop.Application.ApplicationUser.Tests
+{
+    [TestClass()]
+    public class CurrentUserTests
+    {
+        [TestMethod()]
+        public void IsInRole_WithMatchingRole_ShouldReturnTrue()
+        {
+            
+            //arrange
+
+            var currentuser = new CurrentUser("1","test@test.com",new List<string> { "Admin","User" });
+
+            //act 
+            var isInRole = currentuser.IsInRole("Admin");
+
+            //assert
+
+            isInRole.Should().BeTrue();
+
+        }
+
+        [TestMethod()]
+        public void IsInRole_WithNonMatchingCaseRole_ShouldReturnFalse()
+        {
+
+            //arrange
+
+            var currentuser = new CurrentUser("1", "test@test.com", new List<string> { "Admin", "User" });
+
+            //act 
+            var isInRole = currentuser.IsInRole("admin");
+
+            //assert
+
+            isInRole.Should().BeFalse();
+
+        }
+
+        [TestMethod()]
+        public void IsInRole_WithNonMatchingRole_ShouldReturnFalse()
+        {
+            
+            //arrange
+
+            var currentuser = new CurrentUser("1", "test@test.com", new List<string> { "Admin", "User" });
+
+            //act 
+            var isInRole = currentuser.IsInRole("Moderator");
+
+            //assert
+
+            isInRole.Should().BeFalse();
+
+        }
+
+
+    }
+}
---
+++ CarWorkshop.DomainTests/Entities/CarWorkshopTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarWorkshop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CarWorkshop.Domain.Entities.Tests
+{
+    [TestClass()]
+    public class CarWorkshopTests
+    {
+        [TestMethod()]
+        public void EncodeName_ShouldSetEncodedName()
+        {
+            //arrange
+          var carWorkshop = new CarWorkshop();
+            carWorkshop.Name = "Test Workshop";
+
+            //act
+
+            carWorkshop.EncodeName();
+
+            //assert
+
+            carWorkshop.EncodedName.Should().Be("test-workshop");
+
+        }
+
+        [TestMethod()]
+public void EncodeName_ShouldThrowException_WhenNameIsNull()
+        {
+            var carWorkshop = new CarWorkshop();
+
+           Action action = () => carWorkshop.EncodeName();
+
+            action.Invoking(a=>a.Invoke())
+                .Should().Throw<NullReferenceException>();
+
+        }
+
+
+
+
+    }
+}
